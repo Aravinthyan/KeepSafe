53f00483d8f4f3acf54cb4c725780f1151e2c96b
Aravinthyan <43353911+Aravinthyan@users.noreply.github.com>
2021-10-02 07:49:09 +0100

    Add check to see if a password file exists
    Check to see if a password file exists, if not get user to enter a
    master password for the new password file that will be created at the
    end of the program and move onto the next stage.

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"os"
+
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/canvas"
@@ -28,34 +30,77 @@ func main() {
 	window := keepSafe.NewWindow("Keep Safe")
 	window.Resize(fyne.NewSize(500, 700))
 
-	passwordEntry := widget.NewPasswordEntry()
-	passwordEntry.SetPlaceHolder("Enter password...")
+	var masterPassword []byte
+	var content *fyne.Container
 
-	incorrectPassword := canvas.NewText("Incorrect password", color.NRGBA{R: 255, G: 0, B: 0, A: 255})
-	incorrectPassword.Hide()
+	if _, err := os.Stat(database.PasswordFile); os.IsNotExist(err) {
+		passwordEntryOne := widget.NewPasswordEntry()
+		passwordEntryOne.SetPlaceHolder("Enter master password...")
 
-	enterButton := widget.NewButton("Enter", func() {
-		if checkPassword(passwords, []byte(passwordEntry.Text)) {
-		} else {
-			incorrectPassword.Show()
-		}
-	})
+		passwordEntryTwo := widget.NewPasswordEntry()
+		passwordEntryTwo.SetPlaceHolder("Enter master password again...")
 
-	content := container.NewGridWithRows(
-		11,
-		layout.NewSpacer(),
-		layout.NewSpacer(),
-		layout.NewSpacer(),
-		layout.NewSpacer(),
-		passwordEntry,
-		enterButton,
-		incorrectPassword,
-		layout.NewSpacer(),
-		layout.NewSpacer(),
-		layout.NewSpacer(),
-		layout.NewSpacer(),
-	)
+		passwordsDoNotMatch := canvas.NewText("Passwords do not match", color.NRGBA{R: 255, G: 0, B: 0, A: 255})
+		passwordsDoNotMatch.Hide()
+
+		enterButton := widget.NewButton("Enter", func() {
+			if passwordEntryOne.Text == passwordEntryTwo.Text {
+				masterPassword = []byte(passwordEntryOne.Text)
+				passwords.CreateEmptyDB()
+			} else {
+				passwordsDoNotMatch.Show()
+			}
+		})
+
+		content = container.NewGridWithRows(
+			12,
+			layout.NewSpacer(),
+			layout.NewSpacer(),
+			layout.NewSpacer(),
+			layout.NewSpacer(),
+			passwordEntryOne,
+			passwordEntryTwo,
+			enterButton,
+			passwordsDoNotMatch,
+			layout.NewSpacer(),
+			layout.NewSpacer(),
+			layout.NewSpacer(),
+			layout.NewSpacer(),
+		)
+
+	} else {
+
+		passwordEntry := widget.NewPasswordEntry()
+		passwordEntry.SetPlaceHolder("Enter master password...")
+
+		incorrectPassword := canvas.NewText("Incorrect password", color.NRGBA{R: 255, G: 0, B: 0, A: 255})
+		incorrectPassword.Hide()
+
+		enterButton := widget.NewButton("Enter", func() {
+			masterPassword = []byte(passwordEntry.Text)
+			if checkPassword(passwords, masterPassword) {
+			} else {
+				incorrectPassword.Show()
+			}
+		})
+
+		content = container.NewGridWithRows(
+			11,
+			layout.NewSpacer(),
+			layout.NewSpacer(),
+			layout.NewSpacer(),
+			layout.NewSpacer(),
+			passwordEntry,
+			enterButton,
+			incorrectPassword,
+			layout.NewSpacer(),
+			layout.NewSpacer(),
+			layout.NewSpacer(),
+			layout.NewSpacer(),
+		)
+	}
 
 	window.SetContent(content)
 	window.ShowAndRun()
+	passwords.WritePasswords(masterPassword)
 }
